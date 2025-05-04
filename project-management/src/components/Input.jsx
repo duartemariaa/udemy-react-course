56756faeb07fe0d8b38d668e4d80d7a73fd8c3a5
Maria Duarte <mariaduarte061@hotmail.com>
2025-05-04 11:48:56 -0300

    feat: add input component

---
+++ project-management/src/components/Input.jsx
@@ -0,0 +1,12 @@
+import '../index.css'
+
+function Input({label, textarea, ...props}){
+    return (
+        <p>
+            <label>{label}</label>
+            {textarea ? <textarea {...props}/> : <input {...props}/>}
+        </p>
+    )
+}
+
+export default Input
