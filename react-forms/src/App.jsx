2c62aeaf760645b525517ac6dce0b9caf20952c7
Maria Duarte <mariaduarte061@hotmail.com>
2026-01-22 12:32:10 -0300

    feat: create react-forms

---
+++ react-forms/src/App.jsx
@@ -0,0 +1,16 @@
+import Header from "./components/Header.jsx";
+import Login from "./components/Login.jsx";
+import './App.css';
+
+function App() {
+  return (
+    <>
+      <Header/>
+      <main>
+        <Login/>
+      </main>
+    </>
+  )
+}
+
+export default App;
